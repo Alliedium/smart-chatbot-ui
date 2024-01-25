cc9de6445d684e276a336b344ed0ffe573ef9a60
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2024-01-25 16:07:34 +0100

    Update openai.ts
    gpt4 -> gpt-4-1106-preview

--- types/openai.ts
+++ types/openai.ts
@@ -5,7 +5,7 @@ export enum OpenAIModelID {
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_3_5_16K_AZ = 'gpt-35-turbo-16k',
-  GPT_4 = 'gpt-4',
+  GPT_4 = 'gpt-4-1106-preview',
   GPT_4_32K = 'gpt-4-32k',
   TEXT_EMBEDDING_ADA_002 = 'text-embedding-ada-002',
 }
